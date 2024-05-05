c614ea2af52e0c5c0d01bc584ceda68aa3956b82
Pavlo Dzikovskyi <p.dzikovsky@gmail.com>
2024-05-05 22:37:08 +0300

    v0.5.3

--- common/geolib/area.go
+++ common/geolib/area.go
@@ -1,12 +1,9 @@
 package geolib
 
-import (
-	"github.com/pavlo67/common/common/mathlib/plane"
-)
+type Area [2]Point
 
-func XYRangesAround(geoPoint Point, zoom int, hsX, hsY float64) XYRanges {
-
-	tileMin, tileMax := geoPoint.MovedAt(plane.Point2{-hsX, hsY}).Tile(zoom), geoPoint.MovedAt(plane.Point2{hsX, -hsY}).Tile(zoom)
+func (area Area) XYRanges(zoom int) XYRanges {
+	tileMin, tileMax := area[0].Tile(zoom), area[1].Tile(zoom)
 
 	if tileMax.X < tileMin.X {
 		tileMin.X, tileMax.X = tileMax.X, tileMin.X
@@ -16,17 +13,4 @@ func XYRangesAround(geoPoint Point, zoom int, hsX, hsY float64) XYRanges {
 	}
 
 	return XYRanges{Zoom: zoom, XT: XYRange{tileMin.X, tileMax.X}, YT: XYRange{tileMin.Y, tileMax.Y}}
-
-}
-
-func PointInRanges(geoPoint Point, xyRanges XYRanges, tileSide int) plane.Point2 {
-
-	tile := geoPoint.Tile(xyRanges.Zoom)
-	leftTop := tile.LeftTop()
-	rightBottom := Tile{tile.X + 1, tile.Y + 1, tile.Zoom}.LeftTop()
-
-	x := float64(tileSide) * (float64(tile.X-xyRanges.XT[0]) + float64(geoPoint.Lon-leftTop.Lon)/float64(rightBottom.Lon-leftTop.Lon))
-	y := float64(tileSide) * (float64(tile.Y-xyRanges.YT[0]) + float64(geoPoint.Lat-leftTop.Lat)/float64(rightBottom.Lat-leftTop.Lat))
-
-	return plane.Point2{x, y}
 }
--- common/geolib/tile_ranges.go
+++ common/geolib/tile_ranges.go
@@ -2,6 +2,8 @@ package geolib
 
 import (
 	"fmt"
+
+	"github.com/pavlo67/common/common/mathlib/plane"
 )
 
 type XYRange [2]int
@@ -43,4 +45,31 @@ func (xyRanges XYRanges) LeftTop() Point {
 	xyRanges = xyRanges.Canon()
 
 	return Tile{X: xyRanges.XT[0], Y: xyRanges.YT[0], Zoom: xyRanges.Zoom}.LeftTop()
+}
+
+func XYRangesAround(geoPoint Point, zoom int, hsX, hsY float64) XYRanges {
+
+	tileMin, tileMax := geoPoint.MovedAt(plane.Point2{-hsX, hsY}).Tile(zoom), geoPoint.MovedAt(plane.Point2{hsX, -hsY}).Tile(zoom)
+
+	if tileMax.X < tileMin.X {
+		tileMin.X, tileMax.X = tileMax.X, tileMin.X
+	}
+	if tileMax.Y < tileMin.Y {
+		tileMin.Y, tileMax.Y = tileMax.Y, tileMin.Y
+	}
+
+	return XYRanges{Zoom: zoom, XT: XYRange{tileMin.X, tileMax.X}, YT: XYRange{tileMin.Y, tileMax.Y}}
+
+}
+
+func PointInRanges(geoPoint Point, xyRanges XYRanges, tileSide int) plane.Point2 {
+
+	tile := geoPoint.Tile(xyRanges.Zoom)
+	leftTop := tile.LeftTop()
+	rightBottom := Tile{tile.X + 1, tile.Y + 1, tile.Zoom}.LeftTop()
+
+	x := float64(tileSide) * (float64(tile.X-xyRanges.XT[0]) + float64(geoPoint.Lon-leftTop.Lon)/float64(rightBottom.Lon-leftTop.Lon))
+	y := float64(tileSide) * (float64(tile.Y-xyRanges.YT[0]) + float64(geoPoint.Lat-leftTop.Lat)/float64(rightBottom.Lat-leftTop.Lat))
+
+	return plane.Point2{x, y}
 }
